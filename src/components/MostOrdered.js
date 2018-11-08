4f1b127b538d385757b394d1c2e71a04eb67e765
Tony Toussaint <tonyyasi@gmail.com>
2018-11-08 11:19:49 -0600

    Setup of Most_Ordered tab

--- src/components/MostOrdered.js
+++ src/components/MostOrdered.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import {Header} from './Header';
+import {database} from './../config/constants';
 
 export default class MostOrdered extends React.Component {
     currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
+    constructor(props) {
+        super(props);
+
+        let foodOrders = [];
+        let drinksOrders = [];
+        let miscOrders = [];
+
+        database.ref('orders').once('value').then((snapshot) => {
+            const orders = this.snapshotToArray(snapshot.val());
+
+            const deliveredOrders = orders.filter((order) => {
+                return (order.delivered);
+            });
+
+            deliveredOrders.map((order) => {
+                if (order.selectedCategory === 'Food') foodOrders.push(order);
+                if (order.selectedCategory === 'Drinks') drinksOrders.push(order);
+                if (order.selectedCategory === 'Misc') miscOrders.push(order);
+
+            });
+            
+            console.log('food', foodOrders);
+            console.log('drinks', drinksOrders);
+            console.log('misc', miscOrders);
+            // this.setState(() => {
+            //     return {orders:this.openOrders}
+            // });
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+
+    snapshotToArray = snapshot => Object.entries(snapshot).map(e => Object.assign(e[1], { key: e[0] }));
+
 
     render() {
         return (
