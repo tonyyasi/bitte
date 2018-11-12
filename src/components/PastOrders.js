18b5cbdb772da5eb2f3d87e246af03c5b7a033df
Tony Toussaint <tonyyasi@gmail.com>
2018-11-11 22:10:26 -0600

    Add segmented control to Past orders

--- src/components/PastOrders.js
+++ src/components/PastOrders.js
@@ -4,9 +4,12 @@ import {database} from './../config/constants';
 import {OrderList} from './OrderList';
 import { customHistory } from "../index";
 import {validateSession} from '../config/constants';
+import { SegmentedControl } from 'segmented-control';
 
 export default class PastOrders extends React.Component {
     currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    finishedOrders = [];
+    ordersInProgress = [];
 
     constructor(props) {
         super(props);
@@ -18,8 +21,16 @@ export default class PastOrders extends React.Component {
             const userOrders = orders.filter((order) => {
                 return order.orderedById === this.currentUser.uid
             });
+            this.finishedOrders = userOrders.filter((order) => {
+                return order.delivered;
+            });
+
+            this.ordersInProgress = userOrders.filter((order) => {
+                return !order.delivered;
+            });
+            
             this.setState(() => {
-                return {userOrders: userOrders.reverse()}
+                return {userOrders: this.finishedOrders.reverse()}
             });
         }).catch((err) => {
             console.log(err);
@@ -32,12 +43,24 @@ export default class PastOrders extends React.Component {
         }
     }
 
+    handleValueChange = (newValue) => {
+        if (newValue === 'finished') { 
+            this.setState(() => ({userOrders:this.finishedOrders}))
+        } else {
+            this.setState(() => ({userOrders:this.ordersInProgress}))
+        }
+    }
+
     snapshotToArray = snapshot => Object.entries(snapshot).map(e => Object.assign(e[1], { key: e[0] }));
 
     render() {
         return (
             <div>
                 <Header />
+                <SegmentedControl options={[
+                    {label: 'Finished Orders', name: 'finished', value: 'finished', default: true},
+                    {label: 'Orders in progress', name: 'Progress', value: 'inProgress'}
+                ]} setValue={(newValue) => {this.handleValueChange(newValue)}} name="segmented-control" />
                 <OrderList showButtons={false} orders={this.state.userOrders}></OrderList>
 
                 
